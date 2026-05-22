f63ec63da438fbcc953118f6e578cad956d0e803
Khushi Mukhare <khushimukhare299@gmail.com>
2026-05-22 11:11:17 +0530

    Update OrangeHrmTest.java
    Added a sleep delay to allow page loading before interaction.

--- SelProject/src/test/java/test1/OrangeHrmTest.java
+++ SelProject/src/test/java/test1/OrangeHrmTest.java
@@ -14,6 +14,8 @@ public class OrangeHrmTest {
 	  driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 	  
 	  driver.manage().window().maximize();
+
+	  Thread.sleep(5000);
 	  
 	  driver.findElement(By.name("username")).sendKeys("Admin");
 	  driver.findElement(By.name("password")).sendKeys("admin123");
